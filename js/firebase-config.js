fcd213eaa58f938bad13582280841e7d42140da7
atayap <atayap@users.noreply.github.com>
2025-10-03 04:15:10 +0000

    deploy: e9736d2a250c49d97b093db94fd5d84cd3bac5a8

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,19 +1,17 @@
-
 // Import functions from the SDKs
-import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.6.1/firebase-app.js';
-import { getFirestore } from 'https://www.gstatic.com/firebasejs/9.6.1/firebase-firestore.js';
-import { getAuth } from 'https://www.gstatic.com/firebasejs/9.6.1/firebase-auth.js';
-import { getStorage } from 'https://www.gstatic.com/firebasejs/9.6.1/firebase-storage.js';
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-firestore.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-auth.js";
+import { getStorage } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-storage.js";
 
 // Your web app's Firebase configuration
-// Values are injected by GitHub Actions from your repository secrets
 const firebaseConfig = {
-  apiKey: AIzaSyA4aWa520xx0PrinfZ7RCTZL8GDf9U-DNM,
-  authDomain: plahraga.firebaseapp.com,
-  projectId: plahraga,
-  storageBucket: plahraga.firebasestorage.app,
-  messagingSenderId: 1012968248291,
-  appId: 1:1012968248291:web:7939dc609052e4307ac6d6
+  apiKey: "AIzaSyA4aWa520xx0PrinfZ7RCTZL8GDf9U-DNM",
+  authDomain: "plahraga.firebaseapp.com",
+  projectId: "plahraga",
+  storageBucket: "plahraga.firebasestorage.app",
+  messagingSenderId: "1012968248291",
+  appId: "1:1012968248291:web:7939dc609052e4307ac6d6"
 };
 
 // Initialize Firebase
